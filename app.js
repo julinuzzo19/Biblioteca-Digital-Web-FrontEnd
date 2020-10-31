896346ffbf39b47f48e9fd21944a74206c9c801b
julinuzzo19 <julinuzzo19@gmail.com>
2020-10-30 21:25:23 -0300

    app.js fixing

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ const express = require('express');
 const app=express();
 const path=require('path');
 const port = 5500
-const router  = express.Router();
 
 
+app.use(express.json());
+ 
 
-router.get('/',function(req,res){
-res.sendFile(path.join(__dirname+'/home.html'));
-});
 
-app.use('/',router);
+app.use(express.static('public'));
+
 app.listen(port,() => console.log(`Example app listening at http://localhost:${port}`))
 
