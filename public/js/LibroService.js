d8ce4cab0a96a4cead4d414f99b2bd61befdbb43
julinuzzo19 <julinuzzo19@gmail.com>
2020-11-07 01:03:20 -0300

    alquiler and reservas optimized

--- public/js/LibroService.js
+++ public/js/LibroService.js
@@ -47,100 +47,12 @@ export function mostrardatos(lista) {
               <td class="filalibroindex">${fila.titulo}</td>
               <td class="filalibroindex">${fila.autor}</td>
               <td class="filalibroindex">${fila.stock}</td>
-              <td class="filalibroindex"><img id="imagen" src="${
-                fila.imagen
-              }"></td>
-           <td class="filalibroindex"> <input type="button" id="${
-             fila.isbn + "alquiler"
-           }" class="btn btn-lg btn-primary " value="Alquilar"></input>
-            <input type="button" id="${
-              fila.isbn + "reserva"
-            }"  class="btn btn-secondary btn-lg " value="Reservar"></input> </td>`;
+              <td class="filalibroindex"><img id="imagen" src="${fila.imagen}"></td>
+           <td class="filalibroindex"> <input type="button" id="${fila.isbn}" class="btn btn-lg btn-primary " value="Alquilar"></input>
+            <input type="button" id="${fila.isbn}"  class="btn btn-secondary btn-lg " value="Reservar"></input> </td>`;
 
       place.appendChild(element);
     }
-  }
-  /* Realizar Reserva por boton */
-  var botonreserva1 = document.getElementById("1reserva");
-  if (botonreserva1 != null) {
-    botonreserva1.onclick = function () {
-      AlquilerService.reservar("1");
-      alert("Reserva realizada.");
-    };
-  }
-
-  var botonreserva2 = document.getElementById("2reserva");
-  if (botonreserva2 != null) {
-    botonreserva2.onclick = function () {
-      AlquilerService.reservar("2");
-      alert("Reserva realizada.");
-    };
-  }
-
-  var botonreserva3 = document.getElementById("3reserva");
-  if (botonreserva3 != null) {
-    botonreserva3.onclick = function () {
-      AlquilerService.reservar("3");
-      alert("Reserva realizada.");
-    };
-  }
-
-  var botonreserva4 = document.getElementById("4reserva");
-  if (botonreserva4 != null) {
-    botonreserva4.onclick = function () {
-      AlquilerService.reservar("4");
-      alert("Reserva realizada.");
-    };
-  }
-
-  var botonreserva5 = document.getElementById("5reserva");
-  if (botonreserva5 != null) {
-    botonreserva5.onclick = function () {
-      AlquilerService.reservar("5");
-      alert("Reserva realizada.");
-    };
-  }
-
-  /* Realizar alquiler por boton */
-
-  var boton = document.getElementById("1alquiler");
-  if (boton != null) {
-    boton.onclick = function () {
-      AlquilerService.alquilar("1");
-      alert("Alquiler realizado.");
-    };
-  }
-
-  var boton2 = document.getElementById("2alquiler");
-  if (boton2 != null) {
-    boton2.onclick = function () {
-      AlquilerService.alquilar("2");
-      alert("Alquiler realizado.");
-    };
-  }
-
-  var boton3 = document.getElementById("3alquiler");
-  if (boton3 != null) {
-    boton3.onclick = function () {
-      AlquilerService.alquilar("3");
-      alert("Alquiler realizado.");
-    };
-  }
-
-  var boton4 = document.getElementById("4alquiler");
-  if (boton4 != null) {
-    boton4.onclick = function () {
-      AlquilerService.alquilar("4");
-      alert("Alquiler realizado.");
-    };
-  }
-
-  var boton5 = document.getElementById("5alquiler");
-  if (boton5 != null) {
-    boton5.onclick = function () {
-      AlquilerService.alquilar("5");
-      alert("Alquiler realizado.");
-    };
   }
 }
 
@@ -170,3 +82,14 @@ export function SearchByForm() {
 
   getlibrosbyinput(tituloautor);
 }
+
+document.addEventListener("click", (e) => {
+  if (e.target.value === "Reservar") {
+    AlquilerService.reservar(e.target.id);
+    alert("Reserva realizada.");
+  }
+  if (e.target.value == "Alquilar") {
+    AlquilerService.alquilar(e.target.id);
+    alert("Alquiler realizado.");
+  }
+});
